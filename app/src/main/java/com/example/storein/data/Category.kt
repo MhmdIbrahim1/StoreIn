c8eb8c89510d1b9a3518670c0ab158d6b7842669
MhmdIbrahim1 <107378571+MhmdIbrahim1@users.noreply.github.com>
2023-07-17 13:36:43 +0300

    Finish Categories

--- app/src/main/java/com/example/storein/data/Category.kt
+++ app/src/main/java/com/example/storein/data/Category.kt
@@ -6,6 +6,6 @@ sealed class Category(val category: String) {
     object Chair : Category("Chair")
     object Cupboard : Category("Cupboard")
     object Table : Category("Table")
-    object Accessories : Category("Accessories")
+    object Accessories : Category("Accessory")
     object Furniture : Category("Furniture")
 }
